54ff3d41c98efff94be4843787b6c3fe67cf21ef
Liliana <albanska.liliana@itismeucci.com>
2026-09-22 19:36:43 +0200

    Modifica classe Main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,37 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci il numero di cavalli: ");
+        int numeroCavalli = scanner.nextInt();
+
+        int lunghezzaPercorso = 100;
+
+        Cavallo[] cavalli = new Cavallo[numeroCavalli];
+
+        for (int i = 0; i < numeroCavalli; i++) {
+
+            System.out.print("Inserisci il nome del cavallo " + (i + 1) + ": ");
+            String nome = scanner.next();
+
+            cavalli[i] = new Cavallo(nome, lunghezzaPercorso);
+        }
+
+        System.out.println("\n--- INIZIO GARA ---");
+
+        for (int i = 0; i < numeroCavalli; i++) {
+            cavalli[i].start();
+        }
+
+        for (int i = 0; i < numeroCavalli; i++) {
+            cavalli[i].join();
+        }
+
+        System.out.println("-> gara terminata <-");
+        scanner.close();
     }
 }
